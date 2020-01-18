481076bc68cc725befe7a94e65282c937fa91b77
Bryce Beagle <bryce.beagle@dilililabs.com>
2020-01-17 17:59:49 -0800

    Download large files from AWS instead of using GitLFS

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,45 @@
+from pathlib import Path
+import urllib.request
+
 import pytest
 
 from vcap.testing import verify_all_threads_closed
+
+_TEST_IMAGE_DIR = Path("tests/test_images")
+_S3_BASE_URL = "https://open-vision-capsules.s3-us-west-1.amazonaws.com" \
+               "/test-dependencies/{}/{}"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def model_dependencies():
+    dependencies = {
+        "models": {
+            "classification_gait_model.pb":
+                Path("vcap/examples/classifier_gait_example/"),
+            "ssd_mobilenet_v1_coco.pb":
+                Path("vcap/examples/detector_person_example")
+        },
+        "images": [
+            "no_people.jpg",
+            "one_person.jpg",
+            "two_people.jpg"
+        ]
+    }
+
+    # Get models
+    for model_name, model_path in dependencies["models"].items():
+        filepath = model_path / model_name
+        if not filepath.exists():
+            s3_url = _S3_BASE_URL.format("models", model_name)
+            urllib.request.urlretrieve(s3_url, filepath)
+
+    # Get images
+    _TEST_IMAGE_DIR.mkdir(exist_ok=True)
+    for image_name in dependencies["images"]:
+        filepath = _TEST_IMAGE_DIR / image_name
+        if not filepath.exists():
+            s3_url = _S3_BASE_URL.format("images", image_name)
+            urllib.request.urlretrieve(s3_url, filepath)
 
 
 @pytest.fixture(autouse=True, scope="session")
--- vcap/examples/classifier_gait_example/classification_gait_model.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a5cb8a477e607f6d2140ecff34b101bd57577116f9d2c434084be43f8fcf76ef
-size 218062322
