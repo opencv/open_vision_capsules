6aafc89dbf7eada4e63cb5bc85bc6c78d7fcbe0c
Alex <alex.thiel@aotu.ai>
2020-05-14 13:43:02 -0700

    Specified exact package versions to enable a more stable and deterministic environment for capsules.

--- vcap/setup.py
+++ vcap/setup.py
@@ -5,18 +5,18 @@ test_packages = ["pytest", "mock"]
 
 setup(
     name='vcap',
-    version='0.1.1',
+    version='0.1.2',
     description="A library for creating OpenVisionCapsules in Python",
     packages=find_namespace_packages(include=["vcap*"]),
 
     author="Dilili Labs",
 
     install_requires=[
-        "pycryptodomex~=3.0",
-        "scipy~=1.0",
-        "scikit-learn~=0.20",
-        "numpy~=1.18",
-        "tensorflow-gpu~=1.15",
+        "pycryptodomex==3.9.7",
+        "scipy==1.4.1",
+        "scikit-learn==0.22.2",
+        "numpy==1.18.4",
+        "tensorflow-gpu==1.15.2",
     ],
     extras_require={
         "tests": test_packages,
