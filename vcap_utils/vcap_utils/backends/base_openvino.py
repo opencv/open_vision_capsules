0960e8f592aa4cadd988024638580e9435ed2f1b
Alex Thiel <alex.thiel@aotu.ai>
2020-06-12 09:35:47 -0700

    Recording the changes I've made thus far- especially the 'method 2' of batch predict which seems to run slower, but if OpenVINO works on some optimizations, it may be faster.

--- vcap_utils/vcap_utils/backends/base_openvino.py
+++ vcap_utils/vcap_utils/backends/base_openvino.py
@@ -2,13 +2,11 @@ import abc
 import logging
 import os
 from pathlib import Path
-from tempfile import NamedTemporaryFile
 from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 
-from vcap.modifiers import Resize
-from vcap.backend import BaseBackend
+from vcap import Resize, BaseBackend, DetectionNode
 
 
 class BaseOpenVINOBackend(BaseBackend):
@@ -29,9 +27,9 @@ class BaseOpenVINOBackend(BaseBackend):
         """
         super().__init__()
         # Convert from the vcap device naming format openvino format
-        device_name = "CPU" if device_name[:3] == "CPU" else device_name
+        device_name = "CPU" if device_name[:4] == "CPU:" else device_name
 
-        from openvino.inference_engine import IECore
+        from openvino.inference_engine import IECore, StatusCode, WaitMode
 
         self.ie = IECore()
         if cpu_extensions is None:
@@ -57,13 +55,17 @@ class BaseOpenVINOBackend(BaseBackend):
 
         # device_name = "MYRIAD.3.1-ma2480"
         print("Loaded onto device", device_name)
-        n_requests = 40
-        self.oven.MAX_BATCH_SIZE = n_requests
+        # TODO: test num_requests=0, as benchmark.py does
         self.exec_net = self.ie.load_network(
             network=self.net,
-            device_name=f"MULTI:{device_name}",
-            num_requests=n_requests,  # device_name.count(",") + 1
+            device_name=device_name,
+            num_requests=40,
+            config={"EXCLUSIVE_ASYNC_REQUESTS": "YES"}
         )
+        self.oven.MAX_BATCH_SIZE = 40
+        print("nrequests", len(self.exec_net.requests))
+        self.StatusCode = StatusCode
+
         self.output_blob_names: List[str] = list(self.net.outputs.keys())
 
     @classmethod
@@ -113,30 +115,111 @@ class BaseOpenVINOBackend(BaseBackend):
 
         return {input_blob_name: in_frame}, resize
 
-    def batch_predict(self, imgs_bgr: List[np.ndarray]) -> List[object]:
+    def batch_predict_method_2(self, imgs_bgr: List[np.ndarray]) -> List[object]:
         """Use the network for inference. Main entry point for the capsule."""
-        frame_generator = (f for f in imgs_bgr)
+        frame_queue: Deque[np.ndarray] = deque(enumerate(imgs_bgr))
+        # free_request_queue = deque(self.exec_net.requests)
+        requests_in_progress: Dict[int, Tuple[int, Resize]] = {}
+        """A queue of (request_id, (frame_id, resize))"""
+        unsent_results = {}
+        """{frame_id: (results, Resize)}"""
+        next_frame_id = 0
+        """The next frame ID we are awaiting results to send"""
+
+        requests = list(enumerate(self.exec_net.requests))
+        print("Batch", len(imgs_bgr))
+        while (len(frame_queue)
+               + len(unsent_results)
+               + len(requests_in_progress)):
+            self.exec_net.wait(num_requests=1)
+            for rid, request in requests:
+                status = request.wait(0)
+                if status == self.StatusCode.INFER_NOT_STARTED:
+                    # Put another request in the queue, if there are frames
+                    if len(frame_queue):
+                        frame_id, frame = frame_queue.popleft()
+                        feed_dict, resize = self.prepare_inputs(frame)
+                        request.async_infer(feed_dict)
+                        requests_in_progress[rid] = (frame_id, resize)
+
+                if status != self.StatusCode.OK:
+                    continue
+
+                # If this just finished an inference
+                if rid in requests_in_progress:
+                    frame_id, resize = requests_in_progress.pop(rid)
+                    unsent_results[frame_id] = request.outputs, resize
+                # Put another request in the queue, if there are frames
+                if len(frame_queue):
+                    frame_id, frame = frame_queue.popleft()
+                    feed_dict, resize = self.prepare_inputs(frame)
+                    request.async_infer(feed_dict)
+                    requests_in_progress[rid] = (frame_id, resize)
+
+            if next_frame_id in unsent_results:
+                yield self.parse_results(*unsent_results.pop(next_frame_id))
+                next_frame_id += 1
+
+    def batch_predict_method_1(self, input_dicts: List[Dict[str, np.ndarray]]) \
+            -> List[object]:
+        """Use the network for inference. Main entry point for the capsule."""
+        input_generator = (f for f in input_dicts)
+        print("Batch", len(input_dicts))
+        requests = self.exec_net.requests
         while True:
-            requests = []
-            for request in self.exec_net.requests:
+            for request in requests:
                 try:
-                    frame = next(frame_generator)
+                    input_dict = next(input_generator)
                 except StopIteration:
                     if len(requests) == 0:
                         # No more frames to process
                         return
                     # Finish processing the queued request
                     break
-                feed_dict, resize = self.prepare_inputs(frame)
-                requests.append((request, resize))
-                request.async_infer(feed_dict)
+                request.async_infer(input_dict)
 
             for request, resize in requests:
                 request.wait()
                 inference_result = request.outputs
-                results = self.parse_results(inference_result, resize)
-                yield results
+                yield inference_result
+
+    def parse_detection_results(self, results: np.ndarray,
+                                resize: Resize) -> List[DetectionNode]:
+        output_blob_name = self.output_blob_names[0]
+        inference_results = results[output_blob_name]
+
+        input_blob_name = list(self.net.inputs.keys())[0]
+        _, _, h, w = self.net.inputs[input_blob_name].shape
+
+        nodes: List[DetectionNode] = []
+        for result in inference_results[0][0]:
+            # If the first index == 0, that's the end of real predictions
+            # The network always outputs an array of length 200 even if it does
+            # not have that many predictions
+            if result[0] != 0:
+                break
+
+            class_id = round(result[1])
+
+            class_name = self.label_map[class_id]
+
+            x_min, y_min, x_max, y_max = result[3:7]
+            # x and y in res are in terms of percent of image width/height
+            x_min, x_max = x_min * w, x_max * w
+            y_min, y_max = y_min * h, y_max * h
+            coords = [[x_min, y_min], [x_max, y_min],
+                      [x_max, y_max], [x_min, y_max]]
+
+            confidence = float(result[2])
+
+            res = DetectionNode(name=class_name,
+                                coords=coords,
+                                extra_data={"confidence": confidence})
+            nodes.append(res)
+
+        resize.scale_and_offset_detection_nodes(nodes)
+        return nodes
 
     def close(self):
         """Does nothing"""
-        pass
+        self.exec_net.
