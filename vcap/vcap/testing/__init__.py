308f7ad4c52748cf2d3d2966a1af303117d7674b
Alex Thiel <alex.thiel@aotu.ai>
2020-06-15 12:18:49 -0700

    Renamed cpu_loading.py to capsule_loading.py, to better reflect its contents now.

--- vcap/vcap/testing/__init__.py
+++ vcap/vcap/testing/__init__.py
@@ -1,3 +1,3 @@
-from .cpu_loading import load_capsule_with_one_device
+from .capsule_loading import load_capsule_with_one_device
 from .input_output_validation import perform_capsule_tests
 from .thread_validation import verify_all_threads_closed
