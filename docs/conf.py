c34a8f5f9d3b635027dfce21fcd87f0f707c4558
Tyler Compton <xaviosx@gmail.com>
2020-07-01 17:08:20 -0700

    Fix PYTHONPATH when building docs

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../vcap'))
+sys.path.insert(0, os.path.abspath('../vcap_utils'))
 
 
 # -- Project information -----------------------------------------------------
