7a092b317dd5a8ef341a60f915ffafccbbc2fcf1
Tyler Compton <xaviosx@gmail.com>
2020-07-29 11:47:08 -0700

    Make verify_all_threads_closed use regex

--- vcap/vcap/testing/thread_validation.py
+++ vcap/vcap/testing/thread_validation.py
@@ -1,18 +1,35 @@
+import re
 import threading
+from typing import List, Optional
 
 
-def verify_all_threads_closed(allowable_threads=None):
-    """A convenient function to throw an error if all threads are not closed"""
+def verify_all_threads_closed(allowable_threads: Optional[List[str]] = None):
+    """A convenient function to throw an error if all threads are not closed.
+
+    :param allowable_threads: A list of regular expressions that match to
+        thread names that are allowed to stay open
+    """
     if allowable_threads is None:
         allowable_threads = []
-    allowable_threads += ['pydevd.Writer',
-                          'pydevd.Reader',
-                          'pydevd.CommandThread',
-                          'profiler.Reader',
-                          'MainThread']
+    allowable_threads += [
+        r"pydevd\.Writer",
+        r"pydevd\.Reader",
+        r"pydevd\.CommandThread",
+        r"profiler\.Reader",
+        r"MainThread",
+    ]
 
-    open_threads = [t.name for t in threading.enumerate()
-                    if t.name not in allowable_threads]
+    open_threads = []
+
+    for thread in threading.enumerate():
+        matched = False
+        for name_pattern in allowable_threads:
+            if re.match(name_pattern, thread.name):
+                matched = True
+                break
+
+        if not matched:
+            open_threads.append(thread)
 
     if len(open_threads) != 0:
         raise EnvironmentError(
