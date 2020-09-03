a5aa37b47eb9e2aac14a9d4d8c26a4e256b54ace
Tyler Compton <xaviosx@gmail.com>
2020-09-02 17:01:49 -0700

    Package the capsule before loading in test

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from vcap import BaseCapsule
+from vcap import BaseCapsule, package_capsule, CAPSULE_EXTENSION
 from vcap.testing.capsule_loading import load_capsule_with_one_device
 
 
@@ -8,8 +8,14 @@ def test_load_capsule_from_memory():
     """Test that a capsule can be loaded from memory, without any source code
     available.
     """
+    capsule_path = Path("vcap", "examples", "classifier_gait_example")
+    packaged_capsule_path = (capsule_path
+                             .with_name(capsule_path.stem)
+                             .with_suffix(CAPSULE_EXTENSION))
+    package_capsule(capsule_path, packaged_capsule_path)
+
     capsule: BaseCapsule = load_capsule_with_one_device(
-        Path("vcap", "examples", "classifier_gait_example"),
+        packaged_capsule_path,
         from_memory=True)
 
     assert capsule.name == "classifier_gait_example"
