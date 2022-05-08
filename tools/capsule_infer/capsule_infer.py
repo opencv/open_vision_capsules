f703688f13067eafaf0c6a408b4ff7f566f79702
Stephen Li <stephen@aotu.ai>
2022-05-08 15:03:02 -0700

    Print capsule process_frame results

--- tools/capsule_infer/capsule_infer.py
+++ tools/capsule_infer/capsule_infer.py
@@ -81,7 +81,7 @@ def capsule_inference(packaged_capsule_path, unpackaged_capsule_path, image_path
             state=capsule.stream_state(),
         )
         proc_time_ms = (time() - start_time) * 1000
-        print(f"Capsule process frame time {proc_time_ms:0.4f}ms, results: {detection_node}")
+        print(f"Capsule process frame time {proc_time_ms:0.4f}ms")
 
         if detection_node:
             valid_description = capsule.output_type.describes(detection_node)
@@ -102,6 +102,7 @@ def capsule_inference(packaged_capsule_path, unpackaged_capsule_path, image_path
             )
 
         if wait_time is not None:
+            [print(f"Results: {result}") for result in results]
             cv2.imshow("Results", image)
             cv2.waitKey(wait_time)
 
