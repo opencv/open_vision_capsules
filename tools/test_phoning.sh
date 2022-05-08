06649ffbb3f313e6b4d0823c5f5029972438fddb
Stephen Li <stephen@aotu.ai>
2022-05-08 13:03:24 -0700

    Make bulk testing tool more generic

--- tools/test_phoning.sh
+++ tools/test_phoning.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+PROG_PREFIX=~/brainframe
+DATA_PREFIX=~/capsules-test/pictures/2022-04-25/test
+CAPSULE=$PROG_PREFIX/pharmacy/private/classifier_phoning_factory_openvino
+
+IMAGE_TRUE_DIR=phone
+IMAGE_FALSE_DIR=nophone
+ATTRIBUTE=phoning
+DETECTION=person
+
+CAPSULE_INFER_PATH=$PROG_PREFIX/open_vision_capsules/tools/capsule_infer
+CMD="python3 $PROG_PREFIX/open_vision_capsules/tools/capsule_classifier_accuracy/classifier_accuracy.py"
+# CMD=capsule_classifier_accuracy_v0.3
+
+ARGS="--capsule $CAPSULE --images-true $DATA_PREFIX/$IMAGE_TRUE_DIR --images-false $DATA_PREFIX/$IMAGE_FALSE_DIR --data attribute=$ATTRIBUTE detection=$DETECTION"
+
 # Basic while loop
 i=0
 while [ $i -le 9 ]
@@ -6,9 +22,12 @@ do
 	j=0
 	while [ $j -le 9 ]
 	do
-		echo "{\"true threshold\": 0.${i}, \"false threshold\": 0.${j}}" > options.json
-		python3 ~/brainframe/open_vision_capsules/tools/capsule_classifier_accuracy/classifier_accuracy.py --capsule /home/leefr/brainframe/pharmacy/private/classifier_phoning_factory_openvino --images-true /home/leefr/capsules-test/pictures/2022-04-25/test/phone --images-false /home/leefr/capsules-test/pictures/2022-04-25/test/nophone --data attribute=phoning true_threshold="0.$i" false_threshold="0.$j" detection=person
-		#./capsule_classifier_accuracy_v0.3 --capsule /home/leefr/brainframe/pharmacy/private/classifier_phoning_factory_openvino --images-true /home/leefr/capsules-test/pictures/2022-04-25/test/phone --images-false /home/leefr/capsules-test/pictures/2022-04-25/test/nophone --data attribute=phoning true_threshold="0.$i" false_threshold="0.$j" detection=person
+		OPTIONS="{\"true threshold\": 0.${i}, \"false threshold\": 0.${j}}"
+		echo $OPTIONS > options.json
+		echo $OPTIONS
+		echo $CMD $ARGS true_threshold="0.$i" false_threshold="0.$j"
+		export PYTHONPATH=$PYTHONPATH:$CAPSULE_INFER_PATH
+		$CMD $ARGS
 		((j++))
 	done
 	((i++))
